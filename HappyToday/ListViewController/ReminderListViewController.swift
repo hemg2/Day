3d430fb8b2a36be112ca959dc21c08d2a766e287
hemg2 <hemg2@naver.com>
2022-11-06 21:59:54 +0900

    navigation title

--- HappyToday/ListViewController/ReminderListViewController.swift
+++ HappyToday/ListViewController/ReminderListViewController.swift
@@ -22,11 +22,22 @@ class ReminderListViewController: UICollectionViewController {
         dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
-        
+
         updateSnapshot()
-        
         collectionView.dataSource = dataSource
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        let id = reminders[indexPath.item].id
+        showDetail(for: id)
+        return false
+     }
+    
+    func showDetail(for id: Reminder.ID) {
+            let reminder = reminder(for: id)
+            let viewController = ReminderViewController(reminder: reminder)
+            navigationController?.pushViewController(viewController, animated: true)
+        }
     
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
