e44bbe32ff2a4fa566bf8f23031cdba01590bed2
hemg2 <hemg2@naver.com>
2022-11-09 21:19:12 +0900

    DatePicker

---
+++ HappyToday/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  HappyToday
+//
+//  Created by 1 on 2022/11/09.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
